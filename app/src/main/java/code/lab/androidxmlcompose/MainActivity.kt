f673d9e28d769ba448c86dfb77b346fd7f8b58ab
Sanghyun Yi <njsharry32@gmail.com>
2024-03-06 14:40:29 +0900

    feat: add sample text field fun

--- app/src/main/java/code/lab/androidxmlcompose/MainActivity.kt
+++ app/src/main/java/code/lab/androidxmlcompose/MainActivity.kt
@@ -1,14 +1,18 @@
 package code.lab.androidxmlcompose
 
 import android.os.Bundle
-import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import code.lab.androidxmlcompose.ui.theme.AndroidXMLComposeTheme
@@ -26,11 +30,21 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    SampleTextField()
                 }
             }
         }
     }
+}
+
+@Composable
+fun SampleTextField() {
+    var text by remember { mutableStateOf("Text any sentence") }
+
+    TextField(
+        value = text,
+        onValueChange = { text = it },
+        label = { (Text("Label")) })
 }
 
 @Composable
